6829261ee30addbbbf2186ce7ae9cec283b548e7
hendisantika <hendisantika@yahoo.co.id>
2024-01-13 06:18:18 +0700

    Add IP_HEADER_CANDIDATES🫸🌀✏️📗 :octocat:

--- util/HttpRequestResponseUtils.java
+++ util/HttpRequestResponseUtils.java
@@ -12,4 +12,7 @@ public final class HttpRequestResponseUtils {
 
     private HttpRequestResponseUtils() {}
 
+    private static final String[] IP_HEADER_CANDIDATES = { "X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR", "HTTP_X_FORWARDED", "HTTP_X_CLUSTER_CLIENT_IP", "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR", "HTTP_FORWARDED", "HTTP_VIA", "REMOTE_ADDR" };
 }
