66828d734f207ea037b2571e7d398daaa054c7cd
hendisantika <hendisantika@yahoo.co.id>
2024-01-13 06:20:58 +0700

    Add AppConfig🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/onlinevisitortracker/config/AppConfig.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.onlinevisitortracker.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-online-visitor-tracker
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/13/24
+ * Time: 06:20
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig implements WebMvcConfigurer {
+
+    private VisitorLogger visitorLogger;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(visitorLogger);
+    }
+
+}
