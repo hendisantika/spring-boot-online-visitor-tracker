8eb6989669c886c87e084e6ef5f985cec8dc9ec3
hendisantika <hendisantika@yahoo.co.id>
2024-01-13 06:26:04 +0700

    Add greet in GreetingController🫸🌀✏️📗 :octocat:

--- src/main/java/id/my/hendisantika/onlinevisitortracker/controller/GreetingController.java
+++ src/main/java/id/my/hendisantika/onlinevisitortracker/controller/GreetingController.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.onlinevisitortracker.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,5 +21,10 @@ public class GreetingController {
     @GetMapping("/")
     public ResponseEntity<String> home() {
         return new ResponseEntity<>("Welcome to User Activity Logging", HttpStatus.OK);
+    }
+
+    @GetMapping("/hello/{name}")
+    public ResponseEntity<String> greet(@PathVariable String name) {
+        return new ResponseEntity<>("Hello, " + name, HttpStatus.OK);
     }
 }
