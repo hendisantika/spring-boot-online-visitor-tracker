02dd18df29b1cde72274d3d4885c1d492c9a850b
hendisantika <hendisantika@yahoo.co.id>
2024-01-13 06:25:27 +0700

    Add GreetingController🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/onlinevisitortracker/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.onlinevisitortracker.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-online-visitor-tracker
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/13/24
+ * Time: 06:25
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+public class GreetingController {
+}
