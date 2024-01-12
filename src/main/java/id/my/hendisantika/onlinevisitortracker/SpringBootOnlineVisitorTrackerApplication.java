22c9b21c828176ef3b2057c85cb954572f95204c
hendisantika <hendisantika@yahoo.co.id>
2024-01-13 06:12:58 +0700

    Create starter project 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/onlinevisitortracker/SpringBootOnlineVisitorTrackerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.onlinevisitortracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnlineVisitorTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOnlineVisitorTrackerApplication.class, args);
+    }
+
+}
