022d84f2f02ec46451a6b3b12ffb2e50a039d884
hendisantika <hendisantika@yahoo.co.id>
2024-01-13 06:15:40 +0700

    Add Visitor Entity🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/onlinevisitortracker/entity/Visitor.java
@@ -0,0 +1,43 @@
+package id.my.hendisantika.onlinevisitortracker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-online-visitor-tracker
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/13/24
+ * Time: 06:15
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "visitor")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Visitor {
+
+    @Id
+    @GeneratedValue // (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String user;
+    private String ip;
+    private String method;
+    private String url;
+    private String page;
+    private String queryString;
+    private String refererPage;
+    private String userAgent;
+    private LocalDateTime loggedTime;
+    private boolean uniqueVisit;
+}
